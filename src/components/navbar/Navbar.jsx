30234913598c5a28676b77e17c6073f711d8647e
Maxi <maximarelliscg@gmail.com>
2025-09-30 15:17:36 -0300

    actualice 5 archivos

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.scss"
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="lef">Left</div>
+            <div className="right">Right</div>
+
+        </nav>
+      );
+
+
+}
+
+export default Navbar;
