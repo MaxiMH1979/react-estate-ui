a08c42a80392a9d6345ecef100feef16eb6bdda8
Maxi <maximarelliscg@gmail.com>
2025-10-01 18:48:43 -0300

    actualice 4 archivos

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,9 +3,31 @@ import "./navbar.scss"
 function Navbar() {
     return (
         <nav>
-            <div className="lef">Left</div>
-            <div className="right">Right</div>
-
+            <div className="left">
+                <a  href="/" className="logo">
+                    <img src="logo2.png" alt="" />
+                    <span>FG Inmobiliaria</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">Nosotros</a>
+                <a href="/">Contacto</a>
+                <a href="/">Agente</a>
+            </div>
+            <div className="right">
+                <a href="/">Iniciar</a>
+                <a href="/" className="register">Registro</a>
+                <div className="menuIcon">
+                    <img src="/public/menu.png" alt="" />
+            </div>
+            <div className="menu">
+                <a href="/">Home</a>
+                <a href="/">Nosotros</a>
+                <a href="/">Contacto</a>
+                <a href="/">Agente</a>
+                <a href="/">Iniciar</a>
+                <a href="/">Registro</a>
+            </div>
+            </div>
         </nav>
       );
 
