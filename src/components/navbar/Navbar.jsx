53bc8c08c7cea0952f2e9cd6dba2aec23b3e5d8e
Maxi <maximarelliscg@gmail.com>
2025-10-02 02:25:40 -0300

    actualizaron 7 archivos

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,43 @@
-import "./navbar.scss"
+import { useState } from "react";
+import "./navbar.scss";
 
 function Navbar() {
-    return (
-        <nav>
-            <div className="left">
-                <a  href="/" className="logo">
-                    <img src="logo2.png" alt="" />
-                    <span>FG Inmobiliaria</span>
-                </a>
-                <a href="/">Home</a>
-                <a href="/">Nosotros</a>
-                <a href="/">Contacto</a>
-                <a href="/">Agente</a>
-            </div>
-            <div className="right">
-                <a href="/">Iniciar</a>
-                <a href="/" className="register">Registro</a>
-                <div className="menuIcon">
-                    <img src="/public/menu.png" alt="" />
-            </div>
-            <div className="menu">
-                <a href="/">Home</a>
-                <a href="/">Nosotros</a>
-                <a href="/">Contacto</a>
-                <a href="/">Agente</a>
-                <a href="/">Iniciar</a>
-                <a href="/">Registro</a>
-            </div>
-            </div>
-        </nav>
-      );
-
-
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="logo2.png" alt="" />
+          <span>FG Inmobiliaria</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">Nosotros</a>
+        <a href="/">Contacto</a>
+        <a href="/">Agente</a>
+      </div>
+      <div className="right">
+        <a href="/">Iniciar</a>
+        <a href="/" className="register">
+          Registro
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/public/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">Nosotros</a>
+          <a href="/">Contacto</a>
+          <a href="/">Agente</a>
+          <a href="/">Iniciar</a>
+          <a href="/">Registro</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
